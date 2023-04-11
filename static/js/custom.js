f5cf7cf62965c8a6c86554f9943a2c96292b9842
周刘奇 <1379576648@qq.com>
2023-04-11 23:05:37 +0800

    提交

--- static/js/custom.js
+++ static/js/custom.js
@@ -83,7 +83,7 @@ $(document).ready(function() {
     
     // 保存api key与对话数据
     var data = {
-      "apiKey" : "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", // 这里填写固定 apiKey
+      "apiKey" : "", // 这里填写固定 apiKey
     }
    
     // 判断是否使用自己的api key
@@ -100,6 +100,9 @@ $(document).ready(function() {
         data.apiKey = apiKey
       }
 
+    }
+    if (data.apiKey.length==0){
+        data.apiKey = "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";
     }
 
     var message = chatInput.val();
